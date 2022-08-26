bb32ededd87a2fb46e62eebb261e98c44034f761
Narenrios <68191519@unitec.edu.co>
2022-08-26 08:41:08 -0500

    Se agrega el proyecto deprueba

---
+++ trabajo2/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo2.Entities
+{
+    internal class Entity
+    {
+        public string Id { get; protected set; }
+        public string placa { get; protected set; }
+        public Entity(string id, string placa)
+        {
+            Id = id;
+            this.placa = placa;
+        }
+    }
+}
---
+++ trabajo2/Entities/VehiculoGasolina.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo2.Entities
+{
+    internal class VehiculoGasolina : Entity
+    {
+        public VehiculoGasolina(string id, string placa):base(id, placa){}
+
+        public void ImprimirValores()
+        {
+            if(string.IsNullOrEmpty(placa))
+                throw new Exception("")
+            else
+            Console.WriteLine($"{this.Id} {this.placa}");
+        }
+    }
+}
